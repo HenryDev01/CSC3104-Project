b3b65f6d46ebd18f36d6d141f9ea9244fc0f4320
Henry <henryboey39@gmail.com>
2025-10-27 21:59:17 +0800

    Removed unused import

--- backend/src/auth-service/app.py
+++ backend/src/auth-service/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model.Auth import AuthenticationService
 from concurrent import futures
-from Utils.db import get_db_connection
 import auth_pb2_grpc
 import grpc
 import threading
